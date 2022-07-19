c42170b7bb8c700e6322f84f51f3fa9aac4f2998
SangHyeopLee <po3313@gmail.com>
2022-07-19 17:03:29 +0900

    실습

--- error01.py
+++ error01.py
@@ -1,2 +1,13 @@
+# # 문제
+
+# > 두 수를 Input으로 받아 합을 구하는 코드이다.
+# 코드에서 오류를 찾아 원인을 적고, 수정하세요.
+# > 
+
+# ### 오류 코드
+# numbers = input().split()
+# print(sum(numbers))
+
+
 numbers = map(int,input().split())
 print(sum(numbers))
---
+++ error06.py
@@ -0,0 +1,25 @@
+# # 문제
+# > 아래 코드는 문자열에서 모음의 개수를 찾는 코드입니다. 
+# 코드에서 오류를 찾아 원인을 적고, 수정하세요.
+
+### 오류 코드
+# python
+# word = "HappyHacking"
+
+# count = 0
+
+# for char in word:
+#     if char == "a" or "e" or "i" or "o" or "u":
+#         count += 1
+
+# print(count)
+
+word = "HappyHacking"
+
+count = 0
+
+for char in word:
+    if char == "a" or if char == "e" or if char =="i" or if char =="o" or if char =="u":
+        count += 1
+
+print(count)
